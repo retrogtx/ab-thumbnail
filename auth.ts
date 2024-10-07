72e0f40ccb6291301c4bdd89a5c139705b1be509
Amrit Rai <iamamrit27@gmail.com>
2024-10-07 09:45:05 +0530

    fix prisma email error

--- auth.ts
+++ auth.ts
@@ -2,6 +2,29 @@ import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import prisma from "@/lib/prisma"
 import Google from "next-auth/providers/google"
+import { Adapter } from "next-auth/adapters"
+
+interface UserData {
+  name?: string | null
+  email?: string | null
+  image?: string | null
+}
+
+const customAdapter = {
+  ...PrismaAdapter(prisma),
+  createUser: async (user: UserData) => {
+    if (!user.email) {
+      throw new Error("User email is required")
+    }
+    const existingUser = await prisma.user.findUnique({
+      where: { email: user.email },
+    })
+    if (existingUser) {
+      return existingUser
+    }
+    return prisma.user.create({ data: user })
+  },
+} as Adapter
 
 export const {
   handlers,
@@ -9,7 +32,7 @@ export const {
   signIn,
   signOut 
 } = NextAuth({
-  adapter: PrismaAdapter(prisma),
+  adapter: customAdapter,
   providers: [
     Google({
       clientId: process.env.AUTH_GOOGLE_ID,
